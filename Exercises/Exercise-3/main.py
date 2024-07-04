336e5921a3dc0e770e82faeb7b5f67a4982955ab
Manish-KT <104140860+Manish-KT@users.noreply.github.com>
2024-07-05 00:30:39 +0530

    exercise4 complete

--- Exercises/Exercise-3/main.py
+++ Exercises/Exercise-3/main.py
@@ -1,10 +1,42 @@
 import boto3
-
+import gzip
+import os
 
 def main():
-    # your code here
-    pass
+    # Create a session using the UNSIGNED config
+    s3 = boto3.resource('s3')
 
+    # Define bucket and key
+    bucket_name = 'commoncrawl'
+    key = 'crawl-data/CC-MAIN-2022-05/wet.paths.gz'
+
+    # Temporary download location
+    temp_file_loc = "/tmp/"
+
+    # Download the file to a temporary location
+    s3.Object(bucket_name, key).download_file(temp_file_loc + 'wet.paths.gz')
+
+    # Extract the .gz file
+    with gzip.open(temp_file_loc + 'wet.paths.gz', 'rb') as f_in:
+       file_content = f_in.read().decode('utf-8')
+
+    # Write the extracted content to a text file
+    with open(temp_file_loc + 'zipfileContent.txt', 'w') as f_out:
+        f_out.write(file_content)
+
+    # Get the first line from the extracted content
+    first_line = file_content.splitlines()[0]
+    print("First line:", first_line)
+
+    # Download the first file based on the first line
+    s3.Object(bucket_name, first_line).download_file(temp_file_loc + 'first_file.warc.wet.gz')
+
+    # Extract and print the first few lines of the downloaded file
+    with gzip.open(temp_file_loc + 'first_file.warc.wet.gz', 'rb') as f_in:
+       for i, line in enumerate(f_in, start=1):
+           print(line.decode('utf-8').strip())
+           if i >= 5:
+               break
 
 if __name__ == "__main__":
     main()
--- Exercises/Exercise-4/main.py
+++ Exercises/Exercise-4/main.py
@@ -1,9 +1,66 @@
-import boto3
+import os
+import json
 
+
+def open_folders_recursively(path, type="json"):
+    files_path = []
+
+    for root, dirs, files in os.walk(path):
+        print("Current directory:", root)
+
+        for file in files:
+            if file.endswith(f".json"):
+                # print("File:", os.path.join(root, file))
+                files_path.append(os.path.join(root, file))
+    
+    return files_path
+
+
+def dictSerach(dict, final_dict):
+    if isinstance(dict, type(dict)):
+        for key, value in dict.items():
+            if isinstance(value, type(dict)):
+                dictSerach(value, final_dict)
+            else:
+                if key not in final_dict:
+                    final_dict[key] = []
+                final_dict[key].append(value)
 
 def main():
-    # your code here
-    pass
+    start_path = os.getcwd()
+    print("Starting path:", start_path)
+    all_files = open_folders_recursively(start_path, type="json")
+    
+    final_dict = dict()
+
+    # print("All files:", all_files)
+    for path in all_files:
+        with open(path, "r") as f:
+            file = json.load(f)
+            dictSerach(file, final_dict)
+
+    # print("Final dictionary:", final_dict)
+
+    with open("output.txt", "w") as f:
+        # columns
+        for key in final_dict.keys():
+            f.write(key + ",")
+
+        # rows
+        f.write("\n")
+        for i in range(len(final_dict["name"])):
+            for key, value in final_dict.items():
+                try:
+                    f.write(value[i] + ",")
+                except: 
+                    f.write(str(value[i]))
+            f.write("\n")
+        
+    os.rename("output.txt", "output.csv")
+
+
+
+
 
 
 if __name__ == "__main__":
